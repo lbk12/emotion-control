4ff2d7a65cb730732f27b6638aa2addb2f2ea676
Danielle Katz <dkatz@morningstar.com>
2017-03-07 20:07:20 -0600

    basic setup

---
+++ app/main.js
@@ -0,0 +1,5 @@
+'use strict'
+import React from 'react'
+import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {render} from 'react-dom'
+import {connect, Provider} from 'react-redux'
